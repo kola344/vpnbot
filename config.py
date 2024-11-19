a2625acb91257c86d8540e1611c70edadff91fe9
kola <koli>
2024-11-19 18:55:49 +0300

    v1

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 token = '7478269800:AAHdBbgI31_EjbVnBr00CXD4xN3oVA6D0hY'
 channel_id_1 = -1001921232416
 channel_id_2 = -1002132008546
-url = ''
+url = 'kola344-vpnbot-31c6.twc1.net'
 last_client_update = 0
