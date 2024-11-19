26079f585f61cb7ad57849ace2839fa58267b294
kola <koli>
2024-11-19 19:07:57 +0300

    v1

--- channel.py
+++ channel.py
@@ -7,15 +7,13 @@ bot = Bot(token=config.token)
 async def check_user(user_id: int) -> (bool, bool):
     try:
         member = await bot.get_chat_member(chat_id=config.channel_id_1, user_id=user_id)
-        print(member.status.value, user_id)
-        st1 = member.status.value in ("member", "administrator", "creator")
+        st1 = member.status in ("member", "administrator", "creator")
     except TelegramBadRequest as e:
         print(e)
         st1 = False
     try:
         member = await bot.get_chat_member(chat_id=config.channel_id_2, user_id=user_id)
-        print(member.status.value, user_id)
-        st2 = member.status.value in ("member", "administrator", "creator")
+        st2 = member.status in ("member", "administrator", "creator")
     except TelegramBadRequest as e:
         print(e)
         st2 = False
